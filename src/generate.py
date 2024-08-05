94f893b5e153de4c8106e38a35167839cae1fe19
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-08-06 00:40:18 +0100

    feat: generate html page

--- src/generate.py
+++ src/generate.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 import os
 from shutil import rmtree, copy
-# from converter import markdown_to_html_node
+from converter import markdown_to_html_node
 
 
-def recursive_copy(dst: str | Path, src: str | Path):
+_TITLE_PATTERN = "{{ Title }}"
+_CONTENT_PATTERN = "{{ Content }}"
+
+
+def recursive_copy(dst: str | Path, src: str | Path) -> None:
     src_path = Path(src)
     if not src_path.exists():
         raise Exception(f"source path '{src_path}' does not exist")
@@ -33,3 +37,24 @@ def extract_title(markdown: str) -> str:
             return clean_line.removeprefix(prefix)
 
     raise Exception("markdown contains no title")
+
+
+def generate_page(from_path: str | Path, template_path: str | Path, dest_path: str | Path) -> None:
+    print(f"Generating page from {from_path} to {
+          dest_path} using {template_path}")
+    from_path = Path(from_path)
+    template_path = Path(template_path)
+    dest_path = Path(dest_path)
+    if not from_path.exists() or not from_path.is_file():
+        raise Exception("source file does not exist")
+    if not template_path.exists() or not template_path.is_file():
+        raise Exception("template file does not exist")
+
+    content = from_path.read_text()
+    template = template_path.read_text()
+    html_content = markdown_to_html_node(content).to_html()
+    title = extract_title(content)
+    template = template.replace(_TITLE_PATTERN, title)
+    template = template.replace(_CONTENT_PATTERN, html_content)
+    os.makedirs(dest_path.parent, exist_ok=True)
+    dest_path.write_text(template)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import generate
 
 def main():
     generate.recursive_copy('public', 'static')
+    generate.generate_page(
+        'content/index.md', 'template.html', 'public/index.html')
 
 
 main()
