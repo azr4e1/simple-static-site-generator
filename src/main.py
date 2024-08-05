9179d073923302901776f95efd4dea02b6c970d2
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-08-05 21:41:15 +0100

    minor refactor

--- src/main.py
+++ src/main.py
@@ -1,30 +1,8 @@
-from pathlib import Path
-import os
-from shutil import rmtree, copy
-# from converter import markdown_to_html_node
-
-
-def recursive_copy(dst: str | Path, src: str | Path):
-    src_path = Path(src)
-    if not src_path.exists():
-        raise Exception(f"source path '{src_path}' does not exist")
-    dst_path = Path(dst)
-    if dst_path.exists():
-        rmtree(dst_path)
-
-    dst_path.mkdir()
-    content = os.listdir(src)
-    for el in content:
-        full_el_src_path = src_path / Path(el)
-        full_el_dst_path = dst_path / Path(el)
-        if full_el_src_path.is_dir():
-            recursive_copy(full_el_dst_path, full_el_src_path)
-        else:
-            copy(full_el_src_path, full_el_dst_path)
+import utils
 
 
 def main():
-    recursive_copy('public', 'static')
+    utils.recursive_copy('public', 'static')
 
 
 main()
